0efd46e141a589aeaed74e3c76802bef0a4119f9
limon-7 <liibd7@gmail.com>
2024-10-25 01:05:32 +0600

    Refac: modified thread implementation

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -1,42 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Multithreading;
 
-void SingleThread()
-{
-    Console.WriteLine("Single Thread");
-    Console.WriteLine("Task 1: start");
-    PerformTask(3000);
-    Console.WriteLine("Task 1: End");
-
-    Console.WriteLine("Task 2: Start");
-    PerformTask(2000);
-    Console.WriteLine("Task 2: End");
-    
-    Console.WriteLine("End Single Thread");
-
-}
-
- void PerformTask(int delay)
-{
-    Thread.Sleep(delay);
-}
-
-void MultiThread()
-{
-    Console.WriteLine("Multi-Thread");
-
-    Thread t1 = new Thread(() => { PerformTaskWithName("Task-1", 5000); });
-    Thread t2 = new Thread(() => { PerformTaskWithName("Task-2", 2000); });
-    t1.Start();
-    t2.Start();
-}
-void PerformTaskWithName(string taskName, int delay)
-{
-    Console.WriteLine($"{taskName} started");
-    Thread.Sleep(delay);
-    Console.WriteLine($"{taskName} ended");
-}
-
-
-
-SingleThread();
-MultiThread();
+SingleMultiThread.SingleThread();
+SingleMultiThread.MultiThread();
--- Multithreading/SingleMultiThread.cs
+++ Multithreading/SingleMultiThread.cs
@@ -1,6 +1,41 @@
 namespace Multithreading;
 
-public class SingleMultiThread
+public static class SingleMultiThread
 {
+    internal static void SingleThread()
+    {
+        Console.WriteLine("Single Thread");
+        Console.WriteLine("Task 1: start");
+        PerformTask(3000);
+        Console.WriteLine("Task 1: End");
+
+        Console.WriteLine("Task 2: Start");
+        PerformTask(2000);
+        Console.WriteLine("Task 2: End");
     
+        Console.WriteLine("End Single Thread");
+
+    }
+
+   private static void PerformTask(int delay)
+    {
+        Thread.Sleep(delay);
+    }
+
+    internal static void MultiThread()
+    {
+        Console.WriteLine("Multi-Thread");
+
+        Thread t1 = new Thread(() => { PerformWithName("Task-1", 5000); });
+        Thread t2 = new Thread(() => { PerformWithName("Task-2", 2000); });
+        t1.Start();
+        t2.Start();
+    }
+    private static void PerformWithName(string taskName, int delay)
+    {
+        Console.WriteLine($"{taskName} started");
+        Thread.Sleep(delay);
+        Console.WriteLine($"{taskName} ended");
+    }
+
 }
